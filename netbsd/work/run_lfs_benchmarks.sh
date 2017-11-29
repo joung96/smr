346083b0b88f0f9e9a1fbd3ca66dd8cda945b7a0
Jiwon Yuna Joung <joung@college.harvard.edu>
2017-11-28 19:37:57 -0500

    hi

--- netbsd/work/run_lfs_benchmarks.sh
+++ netbsd/work/run_lfs_benchmarks.sh
@@ -1,29 +1,29 @@
 #!/usr/pkg/bin/bash
 
-sudo ./lfs_setup.sh lfs ffs raw reset random-read-4k > fio_output/random-read-4k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-read-16k > fio_output/random-read-16k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-read-256k > fio_output/random-read-256k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-read-1m > fio_output/random-read-1m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-read-8m > fio_output/random-read-8m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-read-16m > fio_output/random-read-16m.txt
+sudo ./hdd_setup.sh hdd lfs raw reset random-read-4k > fio_output/random-read-4k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-read-16k > fio_output/random-read-16k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-read-256k > fio_output/random-read-256k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-read-1m > fio_output/random-read-1m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-read-8m > fio_output/random-read-8m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-read-16m > fio_output/random-read-16m.txt
 
-sudo ./lfs_setup.sh lfs ffs raw reset random-write-4k > fio_output/random-write-4k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-write-16k > fio_output/random-write-16k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-write-256k > fio_output/random-write-256k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-write-1m > fio_output/random-write-1m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-write-8m > fio_output/random-write-8m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset random-write-16m > fio_output/random-write-16m.txt
+sudo ./hdd_setup.sh hdd lfs raw reset random-write-4k > fio_output/random-write-4k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-write-16k > fio_output/random-write-16k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-write-256k > fio_output/random-write-256k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-write-1m > fio_output/random-write-1m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-write-8m > fio_output/random-write-8m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset random-write-16m > fio_output/random-write-16m.txt
 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-read-4k > fio_output/seq-read-4k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-read-16k > fio_output/seq-read-16k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-read-256k > fio_output/seq-read-256k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-read-1m > fio_output/seq-read-1m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-read-8m > fio_output/seq-read-8m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-read-16m > fio_output/seq-read-16m.txt
+sudo ./hdd_setup.sh hdd lfs raw reset seq-read-4k > fio_output/seq-read-4k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-read-16k > fio_output/seq-read-16k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-read-256k > fio_output/seq-read-256k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-read-1m > fio_output/seq-read-1m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-read-8m > fio_output/seq-read-8m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-read-16m > fio_output/seq-read-16m.txt
 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-write-4k > fio_output/seq-write-4k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-write-16k > fio_output/seq-write-16k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-write-256k > fio_output/seq-write-256k.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-write-1m > fio_output/seq-write-1m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-write-8m > fio_output/seq-write-8m.txt 
-sudo ./lfs_setup.sh lfs ffs raw reset seq-write-16m > fio_output/seq-write-16m.txt
+sudo ./hdd_setup.sh hdd lfs raw reset seq-write-4k > fio_output/seq-write-4k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-write-16k > fio_output/seq-write-16k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-write-256k > fio_output/seq-write-256k.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-write-1m > fio_output/seq-write-1m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-write-8m > fio_output/seq-write-8m.txt 
+sudo ./hdd_setup.sh hdd lfs raw reset seq-write-16m > fio_output/seq-write-16m.txt

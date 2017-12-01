b78ad30d24ba311fc674824369690e399b4e6bb6
Jiwon Yuna Joung <joung@college.harvard.edu>
2017-12-01 18:37:06 -0500

    hi

---
+++ netbsd/work/benchmarking/benchmark_smr_ffs.sh
@@ -0,0 +1,29 @@
+#!/usr/pkg/bin/bash
+
+sudo ./smr_setup.sh smr ffs raw reset random-read-4k > fio_output/random-read-4k.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-read-16k > fio_output/random-read-16k.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-read-256k > fio_output/random-read-256k.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-read-1m > fio_output/random-read-1m.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-read-8m > fio_output/random-read-8m.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-read-16m > fio_output/random-read-16m.txt
+
+sudo ./smr_setup.sh smr ffs raw reset random-write-4k > fio_output/random-write-4k.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-write-16k > fio_output/random-write-16k.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-write-256k > fio_output/random-write-256k.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-write-1m > fio_output/random-write-1m.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-write-8m > fio_output/random-write-8m.txt 
+sudo ./smr_setup.sh smr ffs raw reset random-write-16m > fio_output/random-write-16m.txt
+
+sudo ./smr_setup.sh smr ffs raw reset seq-read-4k > fio_output/seq-read-4k.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-read-16k > fio_output/seq-read-16k.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-read-256k > fio_output/seq-read-256k.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-read-1m > fio_output/seq-read-1m.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-read-8m > fio_output/seq-read-8m.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-read-16m > fio_output/seq-read-16m.txt
+
+sudo ./smr_setup.sh smr ffs raw reset seq-write-4k > fio_output/seq-write-4k.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-write-16k > fio_output/seq-write-16k.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-write-256k > fio_output/seq-write-256k.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-write-1m > fio_output/seq-write-1m.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-write-8m > fio_output/seq-write-8m.txt 
+sudo ./smr_setup.sh smr ffs raw reset seq-write-16m > fio_output/seq-write-16m.txt
---
+++ netbsd/work/benchmarking/smr_setup.sh
@@ -0,0 +1,65 @@
+#!/usr/pkg/bin/bash
+
+if [ $# -lt 4 ]
+then
+    echo "Usage: ./setup.sh [smr|hdd] [ffs|lfs] [stl|raw] [reset|noreset] (workload)"
+    exit 1
+fi
+
+sudo umount /mnt/test 2> /dev/null
+
+if [ "$1" == "smr" ]
+then
+    rdev="/dev/wd0a"
+    dev="/dev/dk0"
+
+    if [ "$4" == "reset" ]
+    then
+	# add -z flag for reset
+	flags="-z"
+    else
+	flags=""
+    fi
+fi
+
+if [ "$2" == "lfs" ]
+then
+    label="4.4LFS"
+else
+    label="4.2BSD"
+fi
+
+
+if [ "$3" == "stl" ]
+then
+    sudo ./smr-stl/format --map-bands 7 --group-bands 248 --over-provisioning 1.05 $rdev2
+    #sudo ./smr-stl/format --map-bands 7 --group-bands 5 --over-provisioning 1.05 $rdev2
+    sudo ./stl_attach $dev
+
+    dev="/dev/stl0d"
+    rdev="/dev/rstl0d"
+fi
+
+if [ "$2" == "lfs" ]
+then
+    # sector 4k?
+    sudo newfs_lfs -B256M -f4K -b4K -w32  -F $flags $rdev
+
+    # -n, don't start the cleaner, since we want to use /usr/src3/libexec
+    sudo pkill -9 lfs_cleanerd
+    sudo mount_lfs -n $dev /mnt/test
+    #sudo /usr/src3/libexec/lfs_cleanerd/obj/lfs_cleanerd /mnt/test
+else
+    #sudo /usr/src3/sbin/newfs/obj/newfs -O2 -f4K -b4K -S4096 -s15269888 $rdev
+    sudo newfs $rdev
+    #sudo /usr/src3/sbin/newfs/obj/newfs -O2 -f4K -b4K -S4096 -s509000 $rdev
+
+    sudo mount_ffs $dev /mnt/test
+fi
+
+if [ $# -eq 5 ]
+then
+    #sudo ./fb -f filebench/"$5".f
+    #sudo fio --thread --ioengine=posixaio
+    sudo fio fio_workloads/"$5".fio
+fi
